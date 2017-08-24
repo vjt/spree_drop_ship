65d369ecf7698ff9c9b881b5b6a6a7c10b28c45f
Deepak Vig <deepakvig@gmail.com>
2017-08-24 16:39:17 +0530

    Rails 5.1 - Order Fixed for HasMany Through relationship for Supplier model

--- app/models/spree/supplier.rb
+++ app/models/spree/supplier.rb
@@ -14,12 +14,12 @@ class Spree::Supplier < Spree::Base
     has_many :ckeditor_pictures
     has_many :ckeditor_attachment_files
   end
-  has_many   :products, through: :variants
   has_many   :shipments, through: :stock_locations
   has_many   :stock_locations
   has_many   :supplier_variants
   has_many   :users, class_name: Spree.user_class.to_s
   has_many   :variants, through: :supplier_variants
+  has_many   :products, through: :variants
 
   #==========================================
   # Validations
