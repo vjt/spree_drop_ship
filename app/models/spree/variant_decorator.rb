2012b9b0f15b76eeb154e623211c23dd12a21b3f
Marcello Barnaba <vjt@openssl.it>
2017-10-08 21:33:12 +0200

    Optimize propagation of variants in stock locations

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -9,9 +9,9 @@ module Spree
     private
 
     durably_decorate :create_stock_items, mode: 'soft', sha: '98704433ac5c66ba46e02699f3cf03d13d4f1281' do
-      StockLocation.all.each do |stock_location|
+      StockLocation.where(propagate_all_variants: true).each do |stock_location|
         if stock_location.supplier_id.blank? || self.suppliers.pluck(:id).include?(stock_location.supplier_id)
-          stock_location.propagate_variant(self) if stock_location.propagate_all_variants?
+          stock_location.propagate_variant(self)
         end
       end
     end
