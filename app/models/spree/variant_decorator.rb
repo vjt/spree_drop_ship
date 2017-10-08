d5225c2285bd3ab2e367255b918fcd568d7085ff
Marcello Barnaba <vjt@openssl.it>
2017-10-08 21:33:28 +0200

    Fail when decorated methods are changed

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -8,7 +8,7 @@ module Spree
 
     private
 
-    durably_decorate :create_stock_items, mode: 'soft', sha: '98704433ac5c66ba46e02699f3cf03d13d4f1281' do
+    durably_decorate :create_stock_items, mode: 'strict', sha: '98704433ac5c66ba46e02699f3cf03d13d4f1281' do
       StockLocation.where(propagate_all_variants: true).each do |stock_location|
         if stock_location.supplier_id.blank? || self.suppliers.pluck(:id).include?(stock_location.supplier_id)
           stock_location.propagate_variant(self)
