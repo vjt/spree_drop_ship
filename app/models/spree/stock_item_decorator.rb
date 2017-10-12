5ea0ea77e721282dc069de685d178123e5cfbd75
Marcello Barnaba <vjt@openssl.it>
2017-10-13 00:25:54 +0200

    Fix crash when no supplier variants are defined

--- app/models/spree/stock_item_decorator.rb
+++ app/models/spree/stock_item_decorator.rb
@@ -4,7 +4,7 @@ Spree::StockItem.class_eval do
     supplier_id = self.stock_location.supplier_id
     return unless supplier_id
 
-    self.variant.supplier_variants.where(supplier_id: supplier_id).first!
+    self.variant.supplier_variants.where(supplier_id: supplier_id).first
   end
 
 end
