39795b2138daf58be506bfd9cb4103b1c55adb13
Marcello Barnaba <vjt@openssl.it>
2017-10-14 22:52:03 +0200

    Clean up after upgrade to Spree 3.4

--- app/models/spree/stock_location_decorator.rb
+++ app/models/spree/stock_location_decorator.rb
@@ -5,9 +5,9 @@ Spree::StockLocation.class_eval do
   scope :by_supplier, -> (supplier_id) { where(supplier_id: supplier_id) }
 
   # Wrapper for creating a new stock item respecting the backorderable config and supplier
-  durably_decorate :propagate_variant, mode: 'strict', sha: 'f35b0d8a811311d4886d53024a9aa34e3aa5f8cb' do |variant|
-    if self.supplier_id.blank? || variant.suppliers.pluck(:id).include?(self.supplier_id)
-      self.stock_items.create!(variant: variant, backorderable: self.backorderable_default)
+  durably_decorate :propagate_variant, mode: 'strict', sha: '68fd322da48facbdbf567a2391c2495d04c3c8bc' do |variant|
+    if supplier_id.blank? || variant.suppliers.pluck(:id).include?(supplier_id)
+      stock_items.create!(variant: variant, backorderable: backorderable_default)
     end
   end
 
