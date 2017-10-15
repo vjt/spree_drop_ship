b270728b7cf67d4e187bb786f3a91fc578bc5307
Marcello Barnaba <vjt@openssl.it>
2017-10-16 01:20:44 +0200

    Send suppliers e-mails using proper order hooking

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,48 +3,21 @@ Spree::Order.class_eval do
   has_many :stock_locations, through: :shipments
   has_many :suppliers, through: :stock_locations
 
-  # Once order is finalized we want to notify the suppliers of their drop ship orders.
-  # Here we are handling notification by emailing the suppliers.
-  # If you want to customize this you could override it as a hook for notifying a supplier with a API request instead.
-  def finalize_with_drop_ship!
-    finalize_without_drop_ship!
+  register_update_hook :send_supplier_email
+
+  private
+
+  # Once order is finalized we want to notify the suppliers of their drop ship
+  # orders. Here we are handling notification by emailing the suppliers.
+  #
+  def send_supplier_email
+    return unless  SpreeDropShip::Config[:send_supplier_email]
+
     shipments.each do |shipment|
-      if SpreeDropShip::Config[:send_supplier_email] && shipment.supplier.present?
-        begin
-          Spree::DropShipOrderMailer.supplier_order(shipment.id).deliver!
-        rescue => ex #Errno::ECONNREFUSED => ex
-          puts ex.message
-          puts ex.backtrace.join("\n")
-          Rails.logger.error ex.message
-          Rails.logger.error ex.backtrace.join("\n")
-          return true # always return true so that failed email doesn't crash app.
-        end
-      end
-    end
-  end
-  #alias_method_chain :finalize!, :drop_ship
+      next unless shipment.supplier.present?
 
-end
-
-module FinalizeWithDropShipSupport
-
-  def finalize!
-    super
-    shipments.each do |shipment|
-      if SpreeDropShip::Config[:send_supplier_email] && shipment.supplier.present?
-        begin
-          Spree::DropShipOrderMailer.supplier_order(shipment.id).deliver!
-        rescue => ex #Errno::ECONNREFUSED => ex
-          puts ex.message
-          puts ex.backtrace.join("\n")
-          Rails.logger.error ex.message
-          Rails.logger.error ex.backtrace.join("\n")
-          return true # always return true so that failed email doesn't crash app.
-        end
-      end
+      Spree::DropShipOrderMailer.supplier_order(shipment.id).deliver_later!
     end
   end
 
 end
-
-Spree::Order.send(:prepend, FinalizeWithDropShipSupport)
