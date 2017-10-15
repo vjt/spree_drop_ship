a8015717d06522ddfd4266e779dc9961662a5d4a
Marcello Barnaba <vjt@openssl.it>
2017-10-16 00:29:47 +0200

    Display number of items in suppliers' stock for admins

--- app/models/spree/supplier_variant.rb
+++ app/models/spree/supplier_variant.rb
@@ -6,6 +6,14 @@ module Spree
     after_save    :set_variant_price_from_suppliers_best_price!
     after_destroy :set_variant_price_from_suppliers_best_price!
 
+    def stock_items
+      self.variant.stock_items.where(stock_location: self.supplier.stock_locations)
+    end
+
+    def count_on_hand
+      stock_items.sum(:count_on_hand)
+    end
+
     private
 
     def set_variant_price_from_suppliers_best_price!
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -6,28 +6,46 @@ module Spree
 
     before_create :populate_for_suppliers
 
+    # Returns the price practiced by the given supplier
+    #
     def supplier_price(supplier)
-      supplier_variant = self.supplier_variants.where(supplier_id: supplier).first
-      return unless supplier_variant
+      return unless (supplier_variant = self.supplier_variant(supplier))
 
       price = default_price.dup
       price.amount = supplier_variant.cost
       return price
     end
 
-    # Return the supplier with the lowest price
+    # Returns the count on hand of the given supplier
+    #
+    def supplier_count_on_hand(supplier)
+      return unless (supplier_variant = self.supplier_variant(supplier))
+
+      supplier_variant.count_on_hand
+    end
+
+    # Returns the supplier variant record for the given supplier, or nil
+    # if not found.
+    #
+    def supplier_variant(supplier)
+      self.supplier_variants.where(supplier_id: supplier).first
+    end
+
+    # Returns the supplier with the lowest price
     #
     def best_supplier
       self.suppliers.order("#{SupplierVariant.table_name}.cost").first
     end
 
+    # Sets the product price from the suppliers' lowest price
+    #
     def set_price_from_suppliers_best_price!
       price = self.default_price
       price.amount = best_supplier_price || 0.0
       price.save!
     end
 
-    # Get the best supplier price as of now given available stock items.
+    # Get the lowest supplier price, considering only available stock items
     #
     def best_supplier_price
       self.supplier_variants.joins(:supplier => {:stock_locations => :stock_items}).
